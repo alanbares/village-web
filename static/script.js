2938ecfd707a38d23f969f5a76a6f875dad66b0f
alanbares <neloice@gmail.com>
2015-12-19 14:11:07 -0800

    Refactor village call and connect geolocation with village api

--- static/script.js
+++ static/script.js
@@ -1,17 +1,28 @@
 /*global $ */
 
-// Village api call
-$.get("https://queatz-snappy.appspot.com/api/here?latitude=37.7800958&longitude=-122.4605237&auth=ya29.OwK_gZu6kwBy5Q_N5GkTZvVC1aNJinY4mNl9i3P2joKaXt5UqdFbXusCu0wW1CExbzlEX1U", function (data) {
-    $("#offers").html(data); 
-    console.log(data);
-    var _data = data;
-});
+//Fetch village data
+function fetchVillageData(lat, long) {
+	// Village api call
+	var authKey = "ya29.OwK_gZu6kwBy5Q_N5GkTZvVC1aNJinY4mNl9i3P2joKaXt5UqdFbXusCu0wW1CExbzlEX1U",
+		baseUrl = "https://queatz-snappy.appspot.com/api/here?latitude=",
+		url = baseUrl + lat + "&longitude=" + long + "&auth=" + authKey;
+	$.get(url, function (data) {
+		$("#offers").html(data); 
+		console.log(data);
+		var _data = data;
+	});
+
+}
+
 
 //Geo location call
 function getLocation() {
 	navigator.geolocation.getCurrentPosition(doSomething);
 }
 function doSomething(position) {
-	console.log(position.coords.latitude, position.coords.longitude);
+	var lat = position.coords.lat,
+		long = position.coords.long;
+	console.log(lat, long);
+	fetchVillageData(lat, long)
 }
 getLocation();
