5a802bb889ad997bd6dca8d6c9a2db3b42731e90
alanbares <neloice@gmail.com>
2015-12-18 20:56:08 -0800

    Add style.css and working test call using SF location data

--- static/script.js
+++ static/script.js
@@ -1,4 +1,9 @@
+/*global $ */
+
 // Village api call
-$.get("http://queatz-snappy.appspot.com//api/here?latitude=float&longitude=float", function(data) {
-    $(".result").html(data);
+$.get("https://queatz-snappy.appspot.com/api/here?latitude=37.7800958&longitude=-122.4605237&auth=ya29.OwK_gZu6kwBy5Q_N5GkTZvVC1aNJinY4mNl9i3P2joKaXt5UqdFbXusCu0wW1CExbzlEX1U", function (data) {
+    $("#offers").html(data); 
+    console.log(data);
+    var _data = data;
 });
+
