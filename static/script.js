eee02d68fcfef03310169734940e55c485e524ea
alanbares <neloice@gmail.com>
2015-12-19 14:35:26 -0800

    Fix code error in script.js

--- static/script.js
+++ static/script.js
@@ -1,28 +1,35 @@
 /*global $ */
-
 //Fetch village data
-function fetchVillageData(lat, long) {
+function fetchVillageData(latitude, longitude) {
 	// Village api call
 	var authKey = "ya29.OwK_gZu6kwBy5Q_N5GkTZvVC1aNJinY4mNl9i3P2joKaXt5UqdFbXusCu0wW1CExbzlEX1U",
 		baseUrl = "https://queatz-snappy.appspot.com/api/here?latitude=",
-		url = baseUrl + lat + "&longitude=" + long + "&auth=" + authKey;
+		url = baseUrl + latitude + "&longitude=" + longitude + "&auth=" + authKey;
+
+	console.log("url", url);
+
 	$.get(url, function (data) {
-		$("#offers").html(data); 
-		console.log(data);
-		var _data = data;
+		$("#offers").html(data);
+
+		console.log("data", data);
+
 	});
-
 }
-
 
 //Geo location call
 function getLocation() {
-	navigator.geolocation.getCurrentPosition(doSomething);
+	navigator.geolocation.getCurrentPosition(processGeolocation);
 }
-function doSomething(position) {
-	var lat = position.coords.lat,
-		long = position.coords.long;
-	console.log(lat, long);
-	fetchVillageData(lat, long)
+
+function processGeolocation(position) {
+	console.log("position", position);
+	var latitude = position.coords.latitude,
+		longitude = position.coords.longitude;
+
+	console.log("latitude", latitude);
+	console.log("longitude", longitude);
+
+	fetchVillageData(latitude, longitude)
 }
+
 getLocation();
